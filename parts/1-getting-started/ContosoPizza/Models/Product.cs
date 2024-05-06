7cc07d8a06b65e5e4f20c7113595229bf32e3db3
Douglas S. Elder <douglas.s.elder@boeing.com>
2024-05-06 18:29:25 -0500

    half way through video 1

---
+++ parts/1-getting-started/ContosoPizza/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ContosoPizza.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null;
+    [Column(TypeName = "decimal(6,2")]
+    public decimal Price { get; set; } = 0;
+
+}
